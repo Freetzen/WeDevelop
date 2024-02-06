b688e8e1d6fd96ce3d8315621b9a2b59fc99a8bf
Freetzen <federuizgei@gmail.com>
2024-02-06 19:39:43 -0300

    Modificando Proyecto

---
+++ Frontend/src/components/aboutUs/AboutUs.jsx
@@ -0,0 +1,21 @@
+import work from '../../../public/images/work.jpg'
+import style from './AboutUs.module.css'
+
+const AboutUs = () => {
+  return (
+    <section className={style.aboutUsContainer}>
+        <div>
+          <img src={work} alt="" />
+        </div>
+
+        <div>
+          <p>
+            En WeDevelop, no solo creamos páginas web, sino que también tejemos historias digitales. Desde nuestros modestos comienzos en 2022, nos hemos dedicado apasionadamentre a la creacion de experiencias en línea excepcionales que cautivan, inspiran y conectan
+          </p>
+        </div>
+
+    </section>
+  )
+}
+
+export default AboutUs
