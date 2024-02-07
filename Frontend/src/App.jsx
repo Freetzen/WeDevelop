62ac236fb16d997f142dd334cfc96e21d912f902
lauu <lautaropp03@outlook.com>
2024-02-07 12:31:12 -0300

    redux y componentes nuevos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,9 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/navBar/NavBar'
 import Home from './pages/home/Home'
+import { Quote } from './pages/quote/Quote'
+import ContactUs from './pages/contactUs/ContactUs'
 
 function App() {
 
@@ -8,7 +11,11 @@ function App() {
   return (
     <>
     <NavBar/>
-    <Home/>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/quote" element={<Quote/>}></Route>
+      <Route path="/contact-us" element={<ContactUs/>}></Route>
+    </Routes>
     </>
   )
 }
