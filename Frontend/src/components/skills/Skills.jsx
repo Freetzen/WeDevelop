ea2678864460b4d2fdd89ecfc9f027a6342054b5
RonalReds <ronalrojas.92@gmail.com>
2024-02-07 08:31:30 -0400

    Componente Skill

--- Frontend/src/components/skills/Skills.jsx
+++ Frontend/src/components/skills/Skills.jsx
@@ -1,7 +1,18 @@
+import style from "./Skills.module.css";
+
 
 const Skills = () => {
+
+  const skills = ["Experience", "Creativity", "Attention to detail", "Professional", "Unique desings", "Efficiency"]
+
   return (
-    <div>Skills</div>
+    <div>
+      <h1 className={style.title}>Why choose us?</h1>
+      <div className={style.skillContainer}>
+        {skills.map(skill => <div className={style.skill}>{skill}</div>)}
+        
+      </div>
+    </div>
   )
 }
 
