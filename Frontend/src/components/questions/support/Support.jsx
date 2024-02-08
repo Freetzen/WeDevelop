8cd473ecade754451b0e555355fe48ba5407d3d5
RonalReds <ronalrojas.92@gmail.com>
2024-02-08 16:51:49 -0400

    Componentes: Desing-Suport

--- Frontend/src/components/questions/support/Support.jsx
+++ Frontend/src/components/questions/support/Support.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import style from './Support.module.css'
+import service from './servicios-digitales.png';
+import no from './no.png';
+import si from './si.png';
 
 export const Support = () => {
+
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleYesClick = () => {
+    setShowOptions(true);
+  };
+  const handleNoClick = () => {
+    setShowOptions(false);
+  };
+
   return (
-    <div>Support</div>
-  )
-}
+    <div className={style.container}>
+      <h1>Su web requerirá de soporte?</h1>
+      <div className={style.imageContainer} onClick={handleYesClick}>
+        <img src={service} alt="servicios" className={style.image} />
+        <div>
+          <img src={si} alt="sí" className={style.smallImage} />
+        </div>
+      </div>
+      <div className={style.imageContainer} onClick={handleNoClick}>
+        <img src={service} alt="servicios" className={style.image} />
+        <div>
+          <img src={no} alt="sí" className={style.smallImage} />
+        </div>
+      </div>
+      {showOptions && (
+        <div>
+          <h2>Opciones de soporte</h2>
+          <ul  className={style['options-list']}>
+            <li className={style['option-item']}>Semanal</li>
+            <li className={style['option-item']}>Mensual</li>
+            <li className={style['option-item']}>Trimestral</li>
+            <li className={style['option-item']}>Otros</li>
+          </ul>
+        </div>
+      )}
+      </div>
+  );
+};
--- Frontend/src/components/skills/Skills.jsx
+++ Frontend/src/components/skills/Skills.jsx
@@ -3,13 +3,13 @@ import style from "./Skills.module.css";
 
 const Skills = () => {
 
-  const skills = ["Experience", "Creativity", "Attention to detail", "Professional", "Unique desings", "Efficiency"]
+  const skills = ["Experience", "Creativity", "Attention to detail", "Professional", "Original designs", "Efficiency"]
 
   return (
     <div>
       <h1 className={style.title}>Why choose us?</h1>
       <div className={style.skillContainer}>
-        {skills.map(skill => <div className={style.skill}>{skill}</div>)}
+        {skills.map((skill, index) => <div className={style.skill} key={index}>{skill}<p>Nuestra experiencia en el diseño de páginas web es el resultado de un viaje apasionante y enriquecedor que ha sido impulsado por tu dedicación, creatividad y búsqueda constante de la excelencia.</p></div>)}
         
       </div>
     </div>
