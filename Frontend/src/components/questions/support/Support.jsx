de8423b7b541844e8a37a2f2c0959ec449f1d511
RonalReds <ronalrojas.92@gmail.com>
2024-02-09 08:29:31 -0400

    Desing-Support: ok

--- Frontend/src/components/questions/support/Support.jsx
+++ Frontend/src/components/questions/support/Support.jsx
@@ -4,16 +4,25 @@ import service from './servicios-digitales.png';
 import no from './no.png';
 import si from './si.png';
 
-export const Support = () => {
+export const Support = ({quote, setQuote, question, setQuestion}) => {
 
   const [showOptions, setShowOptions] = useState(false);
 
   const handleYesClick = () => {
     setShowOptions(true);
   };
-  const handleNoClick = () => {
+ 
+
+  const handleClick = (e) => {
+    const valueClick = e.target.value
+    setQuote({
+      ...quote,
+      'support': valueClick
+    })
     setShowOptions(false);
-  };
+    setQuestion(question + 1)
+  }
+
 
   return (
     <div className={style.container}>
@@ -24,7 +33,7 @@ export const Support = () => {
           <img src={si} alt="sí" className={style.smallImage} />
         </div>
       </div>
-      <div className={style.imageContainer} onClick={handleNoClick}>
+      <div className={style.imageContainer} onClick={handleClick}>
         <img src={service} alt="servicios" className={style.image} />
         <div>
           <img src={no} alt="sí" className={style.smallImage} />
@@ -34,10 +43,10 @@ export const Support = () => {
         <div>
           <h2>Opciones de soporte</h2>
           <ul  className={style['options-list']}>
-            <li className={style['option-item']}>Semanal</li>
-            <li className={style['option-item']}>Mensual</li>
-            <li className={style['option-item']}>Trimestral</li>
-            <li className={style['option-item']}>Otros</li>
+            <li className={style['option-item']} onClick={handleClick}>Semanal</li>
+            <li className={style['option-item']} onClick={handleClick}>Mensual</li>
+            <li className={style['option-item']}onClick={handleClick}>Trimestral</li>
+            <li className={style['option-item']}onClick={handleClick}>Otros</li>
           </ul>
         </div>
       )}
