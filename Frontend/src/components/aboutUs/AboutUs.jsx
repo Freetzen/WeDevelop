d07b327dbd707d7c785d8ac73833c229afce7ac1
agusasaad <agusasaad1099@hotmail.com>
2024-02-09 09:32:39 -0300

    home style

--- Frontend/src/components/aboutUs/AboutUs.jsx
+++ Frontend/src/components/aboutUs/AboutUs.jsx
@@ -3,15 +3,17 @@ import style from './AboutUs.module.css'
 const AboutUs = () => {
   return (
     <section className={style.aboutUsContainer}>
+      <div className={style.containerimgAndTitle}>
         <div className={style.aboutUsDivImg}>
-          <img src='./images/work2.jpg' alt="AboutUsImg" />
+          <img src='./images/aboutUs.png' alt="AboutUsImg" />
         </div>
-
         <div className={style.aboutUsDiv}>
+          <h3>About us</h3>
           <p>
             En WeDevelop, no solo creamos páginas web, sino que también tejemos historias digitales. Desde nuestros modestos comienzos en 2022, nos hemos dedicado apasionadamentre a la creacion de experiencias en línea excepcionales que cautivan, inspiran y conectan.
           </p>
         </div>
+      </div>
 
     </section>
   )
--- Frontend/src/components/startQuote/StartQuote.jsx
+++ Frontend/src/components/startQuote/StartQuote.jsx
@@ -2,14 +2,16 @@ import { Link } from 'react-router-dom'
 import style from './StartQuote.module.css'
 const StartQuote = () => {
   return (
-  <>
-    <div className={style.quoteContainer}>
-      <h2 className={style.quoteStartProyect}>Start Proyect</h2>
-      <Link to={"/quote"}>
-      <button className={style.quoteButton}>Quote Now</button>
-      </Link>
-    </div>
-  </>
+    <>
+      <div className={style.quoteContainer}>
+        <div className={style.containerTitle}>
+          <h2 className={style.quoteStartProyect}>Start Proyect</h2>
+        </div>
+        <div className={style.containerButton}>
+          <button className={style.quoteButton}><Link to={"/quote"}>Quote Now</Link></button>
+        </div>
+      </div>
+    </>
   )
 }
 
