2ac3d9d0b645cb59b2cedfa20638023bfa86f4fc
agusasaad <agusasaad1099@hotmail.com>
2024-02-11 20:39:57 -0300

    feedback estilos

--- Frontend/src/components/feedback/Feedback.jsx
+++ Frontend/src/components/feedback/Feedback.jsx
@@ -1,62 +1,63 @@
 import style from './Feedback.module.css'
+import users from './user.js'
 
 const Feedback = () => {
+
   return (
     <div className={style.containerFeedback}>
-      <div className={style.titleFeedback}>
-        <h2>FeedBack</h2>
-      </div>
 
       <div className={style.ratingContainer}>
-
-        <div className={style.NumberContainer}>
-          <h4>1.3</h4>
-          <span>★ ★ ★ ★ ★</span>
-          <p>Average Rating</p>
-        </div>
-
-        <div className={style.BarContainer}>
-          <div className={style.Bar}>
-            <div className={style.Percentage} style={{width: '90%'}}></div>
+        <div className={style.NumberAndCommentsContainer}>
+          <div className={style.NumberContainer}>
+            <h4>4.75</h4>
+            <span>★ ★ ★ ★ ★ ★</span>
+            <p>(1976 Reviews)</p>
           </div>
-          <div className={style.Bar}>
-            <div className={style.Percentage} style={{width: '70%'}}></div>
-          </div>
-          <div className={style.Bar}>
-            <div className={style.Percentage} style={{width: '30%'}}></div>
-          </div>
-          <div className={style.Bar}>
-            <div className={style.Percentage} style={{width: '10%'}}></div>
-          </div>
-          <div className={style.Bar}>
-            <div className={style.Percentage} style={{width: '2%'}}></div>
-          </div>
-        </div>
-
-        <div className={style.startContainer}>
-          <div className={style.startAndPercentage}>
-            <span>★ ★ ★ ★ ☆</span>
-            <p>90%</p>
-          </div>
-          <div className={style.startAndPercentage}>
-            <span>★ ★ ★ ☆ ☆</span>
-            <p>70%</p>
-          </div>
-          <div className={style.startAndPercentage}>
-            <span>★ ★ ☆ ☆ ☆</span>
-            <p>8%</p>
-          </div>
-          <div className={style.startAndPercentage}>
-            <span>★ ☆ ☆ ☆ ☆</span>
-            <p>4%</p>
-          </div>
-          <div className={style.startAndPercentage}>
-            <span>☆ ☆ ☆ ☆ ☆</span>
-            <p>2%</p>
+          <div className={style.barContainer}>
+            <div className={style.container}>
+              <div className={style.Bar} style={{ width: '95%' }}></div>
+            </div>
+            <div className={style.container}>
+              <div className={style.Bar} style={{ width: '75%' }}></div>
+            </div>
+            <div className={style.container}>
+              <div className={style.Bar} style={{ width: '55%' }}></div>
+            </div>
+            <div className={style.container}>
+              <div className={style.Bar} style={{ width: '35%' }}></div>
+            </div>
+            <div className={style.container}>
+              <div className={style.Bar} style={{ width: '15%' }}></div>
+            </div>
           </div>
         </div>
 
-
+        <div className={style.commentsContainer}>
+          <div className={style.title}>
+            <h2>Review and comments </h2>
+          </div>
+          {
+            users.map((user, index) => (
+              <div className={style.comments} key={index}>
+                <div className={style.ImageDateNameAndStartsContainer}>
+                  <div className={style.User}>
+                    <img src={user.urlImage} alt="" style={{ width: '60px', height: '60px', borderRadius: '50%' }} />
+                    <div className={style.NameAndDate}>
+                      <h4>{user.name}</h4>
+                      <p>{user.dayComment}</p>
+                    </div>
+                  </div>
+                  <div className={style.startsContainer}>
+                    <span>{user.stars}</span>
+                  </div>
+                </div>
+                <div className={style.commentP}>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nisi obcaecati id saepe consequuntur explicabo porro qui ab aut sit soluta perspiciatis debitis nihil consequatur doloremque reiciendis quos, est temporibus.</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
