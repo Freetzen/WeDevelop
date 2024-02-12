66c6a6abe729626d3c7c649929b8b36703347315
MauroTre <m.gutierrez7@hotmail.com>
2024-02-12 18:09:08 -0500

    detail

--- Frontend/src/components/details/DetailProyects.jsx
+++ Frontend/src/components/details/DetailProyects.jsx
@@ -1,38 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import proyectsProvider from '../../utils/provider/proyectsProvider/proyectsProvider';
-import style from './Details.module.css';
+import React from "react";
+import { useParams } from "react-router-dom";
+import style from "./ProjectDetails.module.css";
+import { useNavigate } from "react-router-dom";
 
-function DetailProyects() {
-    const [proyects, setProyects] = useState([]);
+export default function ProjectDetails({ projects }) {
+    const { projectId } = useParams();
+    const project = projects.find((p) => p._id === projectId);
 
-    useEffect(() => {
-        async function fetchProyects() {
-            try {
-                const data = await proyectsProvider.getProyects();
-                setProyects(data);
-            } catch (error) {
-                console.error('Error fetching proyects:', error);
-            }
-        }
-        
-        fetchProyects();
-    }, []);
+    if (!project) {
+        return <div>Project not found</div>;
+    }
+
+    const navigate = useNavigate()
+    const backToHome = () => {
+        navigate('/home')
+      }
 
     return (
-        <div>
-            <h1>Proyect Details</h1>
-            <ul>
-                {proyects.map(proyect => (
-                    <li key={proyect.id}>
-                        <p>Name: {proyect.name}</p>
-                        <p>Images: {proyect.images}</p>
-                        <p>Description: {proyect.description}</p>
-                        <p>Category: {proyect.category}</p>
-                    </li>
-                ))}
-            </ul>
+        <div className={style.detailsContainer}>
+            <h2>{project.name}</h2>
+            <img src={project.images} alt={project.name} />
+            <h5>{project.category}</h5>
+            <button onClick={backToHome} className='backToHome'>BACK TO HOME</button>
         </div>
     );
 }
-
-export default DetailProyects;
