48851a2dd412e3714041cb2cda08e743874a328f
matidehoyos <mati.dehoyosmdp@gmail.com>
2024-02-12 06:48:41 -0300

    cargar usuarios en la base de datos

--- Frontend/src/components/loginButton/LoginButton.jsx
+++ Frontend/src/components/loginButton/LoginButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 const LoginButton = () => {
     const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
