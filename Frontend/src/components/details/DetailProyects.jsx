8ecca3d74c5e1ede7cb64eedf4ba7a031922339e
Freetzen <federuizgei@gmail.com>
2024-02-12 20:31:02 -0300

    Solucionando

--- Frontend/src/components/details/DetailProyects.jsx
+++ Frontend/src/components/details/DetailProyects.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import style from "./ProjectDetails.module.css";
+import style from "./DetailProyects.module.css";
 import { useNavigate } from "react-router-dom";
+import projectsProvider from "../../utils/provider/projectsProvider/projectsProvider";
 
-export default function ProjectDetails({ projects }) {
-    const { projectId } = useParams();
-    const project = projects.find((p) => p._id === projectId);
+export default function ProjectDetails() {
+    const { id } = useParams();
 
-    if (!project) {
+    const [projectById, setProjectById] = useState([])
+
+  const getProject = async () => {
+    try {
+      const totalProjects = await projectsProvider.getProjectById(id)
+    setProjectById(totalProjects)
+    console.log(projectById)
+    } catch (error) {
+      console.log(error.message)
+    }
+
+  }
+
+  useEffect(() =>{
+    getProject()
+  }, [])
+
+    if (!id) {
         return <div>Project not found</div>;
     }
 
     const navigate = useNavigate()
     const backToHome = () => {
-        navigate('/home')
+        navigate('/projects')
       }
 
     return (
         <div className={style.detailsContainer}>
-            <h2>{project.name}</h2>
-            <img src={project.images} alt={project.name} />
-            <h5>{project.category}</h5>
+            <h2>{projectById.name}</h2>
+            <img src={projectById.images} alt={projectById.name} />
+            <p>{projectById.description}</p>
+            <h5>{projectById.category}</h5>
             <button onClick={backToHome} className='backToHome'>BACK TO HOME</button>
         </div>
     );
