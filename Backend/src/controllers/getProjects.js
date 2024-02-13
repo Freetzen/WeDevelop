62d84c38ce5164204554e14bcaf0cba269b3d5a3
Freetzen <federuizgei@gmail.com>
2024-02-13 00:28:05 -0300

    Paginate y Filter con Lau

--- Backend/src/controllers/getProjects.js
+++ Backend/src/controllers/getProjects.js
@@ -2,8 +2,9 @@ const { findProject } = require("../services/projectService")
 
 const getProjects = async(req, res) => {
 try {
-    const form = await findProject()
-
+    let page = req.query.page
+    const limit = 10
+    const form = await findProject(page, limit)
     res.status(200).json(form)
 } catch (error) {
     res.status(500).send({ error: error.message })
