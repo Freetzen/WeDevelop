b7a43ee219d826d7402f6b0af4727bc6ac250d3b
YZeaFons <yeisonyamidzea@gmail.com>
2024-02-13 10:11:37 -0500

    En el nombre del padre, del hijo...

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,25 +9,29 @@ import AdminWindow from './pages/adminWindow/AdminWindow'
 import CreateProject from './components/adminUtils/createProject/CreateProject'
 import ProjectDetails from './components/details/DetailProyects'
 import Pricing from './components/pricing/Pricing'
+import UserAdmin from './components/adminUtils/usersAdmin/UserAdmin'
+import NotFound from './pages/notFound/NotFound'
 
 
-    
+
 function App() {
 
   return (
     <>
 
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/quote" element={<Quote/>}></Route>
-      <Route path="/contact" element={<ContactUs/>}></Route>
-      <Route path="/pricing" element={<Pricing/>}></Route>
-      <Route path="/projects" element={<Projects/>}></Route>
-      <Route path="/projects/:id" element={<ProjectDetails/>}></Route>
-      <Route path="/admin" element={<AdminWindow />}></Route>
-      <Route path="/createProject" element={<CreateProject />}></Route>
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/quote" element={<Quote />}></Route>
+        <Route path="/contact" element={<ContactUs />}></Route>
+        <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/projects/:id" element={<ProjectDetails />}></Route>
+        <Route path="/admin" element={<AdminWindow />}></Route>
+        <Route path="/createProject" element={<CreateProject />}></Route>
+        <Route path="/createUser" element={<UserAdmin />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
 
     </>
   )
