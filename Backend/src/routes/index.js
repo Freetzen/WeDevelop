9cf5c0fee54ec7a755a24f2f05cc78014092b654
matidehoyos <mati.dehoyosmdp@gmail.com>
2024-02-15 07:03:31 -0300

    commit para poder pullear

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,12 +3,15 @@ const postUsers = require('../controllers/postUsers');
 const postProject = require('../controllers/postProject');
 const getUsers = require('../controllers/getUsers');
 const postAdmin = require('../controllers/postAdmin');
+const postReviews = require('../controllers/postReviews');
+const getReviews = require('../controllers/getReviews');
 const getUserByEmail = require('../controllers/getUserByEmail');
 const getProjects = require('../controllers/getProjects');
 const getProjectById = require('../controllers/getProjectById');
 const putProject = require('../controllers/putProject');
 const getProjectByName = require('../controllers/getProjectByName');
 const getProjectByCategory = require('../controllers/getProjectByCategory');
+const getReviewAvarage = require('../controllers/getReviewAvarage');
 const router = express.Router();
 
 router.get('/projects/category', getProjectByCategory)
@@ -21,5 +24,8 @@ router.post('/login', postUsers)
 router.post('/projects', postProject)
 router.post('/admin', postAdmin)
 router.put('/projects', putProject)
+router.post('/reviews', postReviews);
+router.get('/reviews', getReviews);
+router.get('/reviewsRating', getReviewAvarage);
 
 module.exports = router
---
+++ Frontend/src/components/review/Review.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import ReviewForm from "../reviewForm/ReviewForm";
+import ReviewCard from "../reviewCard/ReviewCard";
+import style from "./Review.module.css";
+import ReviewRating from "../reviewRating/ReviewRating";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function Review() {
+    const { isAuthenticated } = useAuth0();
+    const [reviews, setReviews] = useState({});
+    const [messages, setMessages] = useState([]);
+    const message = messages.filter(message => message.date);
+
+    const handleReviewSubmit = (review) => {
+      setReviews(review);
+    };
+
+  
+
+    useEffect(() => {
+      const fetchMessages = async () => {
+        try {
+          const response = await axios.get('http://localhost:3001/reviews');
+          setMessages(response.data);
+        } catch (error) {
+          console.error('Error al recuperar los mensajes:', error);
+        }
+      };
+      fetchMessages();
+    },[]);
+
+    useEffect(() => {
+      const sendReviewsToServer = async () => {
+        try {
+            const response = await axios.post('http://localhost:3001/reviews', reviews);
+            console.log('Datos enviados al servidor:', response.data);
+         } catch (error) {
+          console.error('Error al enviar datos al servidor:', error);
+        }
+      };
+      if (reviews) {
+        sendReviewsToServer();
+      }
+    }, [reviews]);
+  
+    return (
+      <div className={style.container}>
+        <div><ReviewRating /></div>
+        <h2>Reviews and comments</h2>
+        <div className={style.cardContainer}>
+          {message.map((review, index) => (
+          <ReviewCard key={index} review={review} />
+        ))}
+        </div>
+
+        { isAuthenticated ?
+        <div>
+          <h2>Leave us a review</h2>
+          <ReviewForm onSubmit={handleReviewSubmit} />
+        </div>
+        : <h3>You must be singed up to send review</h3>
+        }
+      </div>
+    );
+}
