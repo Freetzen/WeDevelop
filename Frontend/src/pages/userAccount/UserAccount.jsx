b4fa9a42e2d1d2bc4e627d5261433332fa2d608d
lauu <lautaropp03@outlook.com>
2024-02-15 08:52:30 -0300

    e

--- Frontend/src/pages/userAccount/UserAccount.jsx
+++ Frontend/src/pages/userAccount/UserAccount.jsx
@@ -4,7 +4,7 @@ import style from "./UserAccount.module.css"
 export const UserAccount = () => {
     const { user, logout } = useAuth0()
     console.log(user)
-    // let arr = Object.keys(user)
+    // // let arr = Object.keys(user)
     let fecha = user.updated_at.split("")
     let res = fecha.slice(0,10)
   return (<>
@@ -12,14 +12,9 @@ export const UserAccount = () => {
         <h2>{user.name}</h2>
         <h2>{user.email}</h2>
         <h2>Plan/membresia:</h2>
-        <h2>fecha de creacion de la cuenta: {user.updated_at}</h2>
+        <h2>fecha de creacion de la cuenta: {res}</h2>
         <button onClick={() => logout()}>LogOut: {user.name}</button>
-    {/* {
-        arr.map((e)=>
-            <h2>{e}: {user[e]}</h2>
-        )
-        
-    } */}
+    
     </>
   )
 }
