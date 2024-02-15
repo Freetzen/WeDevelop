ccb3314fdf4d048c19f1c9e13e65eb2b0d517d19
lauu <lautaropp03@outlook.com>
2024-02-15 10:11:44 -0300

    e

--- Frontend/src/pages/userAccount/UserAccount.jsx
+++ Frontend/src/pages/userAccount/UserAccount.jsx
@@ -1,16 +1,48 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import style from "./UserAccount.module.css"
+import { useEffect, useState } from "react";
+import userProvider from "../../utils/provider/userProvider/userProvider";
 
 export const UserAccount = () => {
+  // const [changes, setChanges] = useState({});
     const { user, logout } = useAuth0()
     console.log(user)
-    // // let arr = Object.keys(user)
     let fecha = user.updated_at.split("")
     let res = fecha.slice(0,10)
+
+//     useEffect(()=>{
+//       const fetchData = async () => {
+//       try {
+//         const user = await userProvider.getUserByEmail(user.email);
+//         setChanges(user);
+//       } catch (error) {
+//         console.log(error.message)
+//       }
+//     }
+//     fetchData();
+//     },[])
+//     const handleChange = (e) => {
+//       setChanges({
+//           ...changes,
+//           [e.target.name]: e.target.value
+//       })
+//   }
+
+//   const sendUserChanges = async () => {
+//     try {
+//       await userProvider.putUser(changes);
+//       const usersResponse = await userProvider.getUsers()
+//     } catch (error) {
+//       console.log(error.message)
+//     }
+// }
+//   console.log(changes.name)
   return (<>
-        <img src={user.picture}></img>
-        <h2>{user.name}</h2>
-        <h2>{user.email}</h2>
+        <img src={user?.picture}></img>
+        <label>{user?.name}</label>
+        <input type="text" name="name" value={changes.name} onChange={handleChange}/>
+        <button onClick={sendUserChanges}>save changes</button>
+        <h2>{user?.email}</h2>
         <h2>Plan/membresia:</h2>
         <h2>fecha de creacion de la cuenta: {res}</h2>
         <button onClick={() => logout()}>LogOut: {user.name}</button>
