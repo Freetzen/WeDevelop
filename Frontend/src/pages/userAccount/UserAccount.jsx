fe78ca1dca0a83585ca41154017fae3a2915624b
lauu <lautaropp03@outlook.com>
2024-02-15 22:20:54 -0300

    agregado de botones y diseño del useraccount lauluna

--- Frontend/src/pages/userAccount/UserAccount.jsx
+++ Frontend/src/pages/userAccount/UserAccount.jsx
@@ -2,6 +2,8 @@ import { useAuth0 } from "@auth0/auth0-react"
 import style from "./UserAccount.module.css"
 import { useEffect, useState } from "react";
 import userProvider from "../../utils/provider/userProvider/userProvider";
+import { Link } from "react-router-dom";
+
 
 export const UserAccount = () => {
     const { user, logout } = useAuth0()
@@ -20,13 +22,16 @@ export const UserAccount = () => {
     },[])
     
   return (<>
+        <Link to="/">
+        <button>Home</button>
+        </Link>
         <img src={user?.picture}></img>
-        <label>{user?.name}</label>
-        <h2>{user?.email}</h2>
+        <h2>Nombre: {user?.name}</h2>
+        <h2>Email: {user?.email}</h2>
         <h2>Plan/membresia:</h2>
         <h2>fecha de creacion de la cuenta: {res}</h2>
         <button onClick={() => logout()}>LogOut: {user.name}</button>
-    
+
     </>
   )
 }
