746b145ebbc57094424b211a36ca73c078a268d7
Freetzen <federuizgei@gmail.com>
2024-02-17 19:23:46 -0300

    Boton Admin

--- Frontend/src/pages/userAccount/UserAccount.jsx
+++ Frontend/src/pages/userAccount/UserAccount.jsx
@@ -1,27 +1,26 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import style from "./UserAccount.module.css"
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import userProvider from "../../utils/provider/userProvider/userProvider";
 
 
-export const UserAccount = ({menuIsActive, activeMenu}) => {
+export const UserAccount = ({ menuIsActive }) => {
   const { user, logout } = useAuth0()
+  const [userBD, setUserBD] = useState({})
   let fecha = user.updated_at.split("")
   let res = fecha.slice(0, 10)
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { user } = await userProvider.getUserByEmail(user.email);
-      } catch (error) {
-      }
-    }
     fetchData();
   }, [])
-
+  const fetchData = async () => {
+    const userext = await userProvider.getUserByEmail(user.email);
+    setUserBD(userext)
+  }
 
   return (
-    <div className={style.infoContainer} style={menuIsActive ? {left: '-20%'} : {left: '0%'}}>
+    <div className={style.infoContainer} style={menuIsActive ? { left: '-20%' } : { left: '0%' }}>
       <div className={style.imgAndNameContainer}>
         <img src={user?.picture}></img>
         <h2>{user?.name}</h2>
@@ -35,6 +34,16 @@ export const UserAccount = ({menuIsActive, activeMenu}) => {
       </div>
       <div className={style.buttonsContainer}>
         <button onClick={() => logout()}>Sign Out</button>
+        <div>
+          {
+            userBD.role === 'admin'
+              ?
+              <Link to={'/admin'}>
+                <button>Admin panel</button>
+              </Link>
+              : null
+          }
+        </div>
       </div>
     </div>
   )
