62854012f7c34c647e950de0d4af9058709e50aa
MauroTre <m.gutierrez7@hotmail.com>
2024-02-19 16:35:46 -0500

    banned

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -1,33 +1,37 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    image: {
-        type: String,
-        required: false
-    },
-    role: {
-        type: String,
-        default: "user"
-    },
-    suspended: {
-        type: Boolean,
-        default: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  suspended: {
+    type: Boolean,
+    default: false,
+  },
+  banned: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const userModel = model("users", userSchema)
+const userModel = model("users", userSchema);
 
-module.exports =  userModel
+module.exports = userModel;
