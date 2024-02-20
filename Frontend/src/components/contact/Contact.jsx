d1b90ea6484070415686e5b53a75bc36260dbf53
Freetzen <federuizgei@gmail.com>
2024-02-19 21:16:05 -0300

    globales

--- Frontend/src/components/contact/Contact.jsx
+++ Frontend/src/components/contact/Contact.jsx
@@ -1,47 +1,63 @@
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-import style from './Contact.module.css'
+import React, { useRef } from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import style from "./Contact.module.css";
+import Swal from "sweetalert2";
 
 const Contact = () => {
-
   const [state, handleSubmit] = useForm("xrgnqgnz");
-  if (state.succeeded) {
-    return <p>Message has been sent!!</p>;
-  }
+  const formRef = useRef(null); 
+
+  const handleFormSubmit = async (event) => {
+    await handleSubmit(event);
+    if (state.succeeded) {
+      Swal.fire({
+        title: "Message has been sent!!",
+        text: "We will contact you very soon.",
+        icon: "success"
+      });
+      formRef.current.reset(); 
+    }
+  };
+
   return (
     <div className={style.containerContactUs}>
       <div className={style.flyerWebDevelop}>
         <div className={style.containerImage}>
-          <img src='./images/logo-nav.png' alt="" />
+          <img src="./images/logo-nav.png" alt="" />
           <span>By developers</span>
         </div>
       </div>
-      <form onSubmit={handleSubmit} className={style.form}>
+      <form ref={formRef} onSubmit={handleFormSubmit} className={style.form}>
         <div className={style.titleProject}>
           <h3>Contact Us</h3>
         </div>
-        <label htmlFor="email">Name</label>
+        <label htmlFor="name">Name</label>
         <input id="name" type="text" name="name" />
         <ValidationError prefix="Name" field="text" errors={state.errors} />
-
 
         <label htmlFor="email">Email Address</label>
         <input id="email" type="email" name="email" />
         <ValidationError prefix="Email" field="email" errors={state.errors} />
 
-        
-        <textarea id="message" name="message" placeholder="Escribe tu mensaje aquí..." />
-        <ValidationError prefix="Message" field="message" errors={state.errors} />
-        
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Write your message here..."
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
 
         <div className={style.containerButton}>
-        <button type="submit" disabled={state.submitting}>
-          Submit
-        </button>
+          <button type="submit" disabled={state.submitting}>
+            Submit
+          </button>
         </div>
       </form>
     </div>
   );
-}
+};
 
-export default Contact
+export default Contact;
