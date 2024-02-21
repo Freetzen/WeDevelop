e9a14b6d5e50c91598768b69eddc9ccbacd53549
Freetzen <federuizgei@gmail.com>
2024-02-21 14:12:18 -0300

    Pasarela de Pago

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -9,10 +9,25 @@ import StartQuote from "../../components/startQuote/StartQuote";
 import SpinnerConLogo from '../../components/spinners/spinnerConLogo/SpinnerConLogo';
 
 const Home = () => {
+  const [chargingScreen, setChargingScreen] = useState(true);
+  const [loadingComplete, setLoadingComplete] = useState(false);
+
+  useEffect(() => {
+    const pantallaDeCarga = () => {
+      setTimeout(() => {
+        setChargingScreen(false);
+        setLoadingComplete(true);
+      }, 5000);
+    };
+
+    if (!loadingComplete) {
+      pantallaDeCarga();
+    }
+  }, []);
 
   return (
     <>
-      
+      {chargingScreen ? <SpinnerConLogo /> :
         <>
           <Header />
           <AboutUs />
@@ -22,7 +37,7 @@ const Home = () => {
           <Review />
           <Footer />
         </>
-      
+      }
     </>
   );
 };
