688809d754c67440b30c0686c385cdf826d96645
Freetzen <federuizgei@gmail.com>
2024-02-21 22:23:45 -0300

    probando MP 2

--- Backend/src/controllers/postMercadoPago.js
+++ Backend/src/controllers/postMercadoPago.js
@@ -6,7 +6,11 @@ try {
 
     const {data} = req.body
 
-    const payment = await axios(`https://api.mercadopago.com/v1/payments/${data.id}`)
+    const {id} = data
+
+    console.log('DATA.ID: ', id)
+
+    const payment = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`)
     
     console.log('PAYMENT: ', payment)
 
