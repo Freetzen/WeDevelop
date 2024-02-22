925e3a2a67c0fa3e99237bfef1a46457af85ebdf
RonalReds <ronalrojas.92@gmail.com>
2024-02-22 11:02:01 -0400

    LocalStorage en Contact

--- Frontend/src/components/contact/Contact.jsx
+++ Frontend/src/components/contact/Contact.jsx
@@ -2,10 +2,17 @@ import React, { useRef } from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import style from "./Contact.module.css";
 import Swal from "sweetalert2";
+import { useLocalStorage } from "./useLocalStorage";
+
 
 const Contact = () => {
   const [state, handleSubmit] = useForm("xrgnqgnz");
   const formRef = useRef(null); 
+
+  const [email, setEmail] = useLocalStorage('email', '')
+  const [name, setName] = useLocalStorage('name', '')
+  const [message, setMessage] = useLocalStorage('message', '')
+
 
   const handleFormSubmit = async (event) => {
     await handleSubmit(event);
@@ -32,17 +39,19 @@ const Contact = () => {
           <h3>Contact Us</h3>
         </div>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" name="name" />
+        <input id="name" type="text" name="name" value={name} onChange={e => setName(e.target.value)}/>
         <ValidationError prefix="Name" field="text" errors={state.errors} />
 
         <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email"  value={email} onChange={e => setEmail(e.target.value)}/>
         <ValidationError prefix="Email" field="email" errors={state.errors} />
 
         <textarea
           id="message"
           name="message"
           placeholder="Write your message here..."
+          value={message}
+          onChange={e => setMessage(e.target.value)}
         />
         <ValidationError
           prefix="Message"
