e3d92793548fba7bcdb0065381921b334becf6c9
lauu <lautaropp03@outlook.com>
2024-02-29 00:35:40 -0300

    localStorage terminado

--- Frontend/src/helpers/local.js
+++ Frontend/src/helpers/local.js
@@ -1,6 +1,6 @@
 export const userDate = (key, elements) => {
     const getUserLocal = JSON.parse(localStorage.getItem('info'));
-    if(!getUserLocal){
+    if(!getUserLocal?.email){
         const setUserLocal = localStorage.setItem(key, JSON.stringify(elements))   
         return setUserLocal
     }
